b6dae83b7b71164ea19009c0ea7ce681061ad54a
Uniliva Alves <Uniliva-ap@hotmail.com>
2016-11-14 10:35:19 -0200

    Commit inicial
    Montada Estrutura e tela de login criada

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Login - Falaê</title>
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+  <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
+<link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Montserrat:400,700'>
+<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+  
+    <div class="container">
+      <div class="info">
+		
+        <h1>Falaê</h1><span>Falaê e um sistema de comunicação desenvolvido pelos alunos da fatec de Osasco.<br/>
+          Atraves do Falaê é possivel postar mensagens. ler o que seus amigos estão dizendo e curtir odiar as mensagens.</span>
+      </div>
+    </div>
+    <div class="form">
+      <div class="thumbnail"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/hat.svg"/></div>
+      <form class="register-form">
+        <input type="text" placeholder="E-mail"/>
+        <input type="text" placeholder="Nick"/>
+        <input type="password" placeholder="Senha"/>
+        <input type="text" placeholder="Status"/>
+        <button>Criar</button>
+        <p class="message">Tem uma conta? <a href="#">Entre</a></p>
+      </form>
+      <form class="login-form">
+        <input type="text" placeholder="E-mail"/>
+        <input type="password" placeholder="Senha"/>
+        <button>Entrar</button>
+        <p class="message">Não esta registrado? <a href="#">Cadastre-se</a></p>
+      </form>
+    </div>
+    
+      <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+        <script src="js/index.js"></script>
+
+</body>
+</html>
